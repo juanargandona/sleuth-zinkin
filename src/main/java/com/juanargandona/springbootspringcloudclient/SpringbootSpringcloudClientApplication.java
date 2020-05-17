7b840a0afe53b72b2cb9f7124ed88eaa057d4b58
Juan Argandoña <juanargandona@gmail.com>
2020-05-17 12:14:41 -0500

    Adding new project

---
+++ src/main/java/com/juanargandona/springbootspringcloudclient/SpringbootSpringcloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.juanargandona.springbootspringcloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringcloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringcloudClientApplication.class, args);
+	}
+
+}
